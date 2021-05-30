5d8d81f7344998b63ba6c68cd174f593fe7723c4
Junya Murakami <junyamurakami719@yahoo.co.jp>
2021-05-30 13:02:55 +0900

    Fixed Bug
    手札枚数が初動札でないカードの数を上回る場合の不具合の修正。

--- script.js
+++ script.js
@@ -23,11 +23,23 @@ function calculate(){
   var target_count = $("#target_card_count").val();
   var hand_count = $("#hand_count").val();
   
-  var result = combinations(deck_count-target_count, hand_count);
-  result /= combinations(deck_count, hand_count);
-  result *= 10000;
-  result = Math.ceil(result);
-  result /= 100;
+  var result = 0;
+  
+  if(target_count == 0 ){
+    result = 100;
+  }
+  
+  else if( deck_count == hand_count || deck_count - hand_count < target_count ){
+    result = 0;
+  }
+  
+  else {
+    result = math.combinations(deck_count-target_count, hand_count);
+    result /= math.combinations(deck_count, hand_count);
+    result *= 1000000;
+    result = Math.floor(result);
+    result /= 10000;
+  }
   
   result += "%";
   
@@ -42,26 +54,4 @@ function adjust_max(){
   $("#hand_count").attr({
     "max" : $("#deck_count").val()  
   });
-}
-
-function product_Range(a,b) {
-  var prd = a,i = a;
- 
-  while (i++< b) {
-    prd*=i;
-  }
-  return prd;
-}
-
-function combinations(n, r) 
-{
-  if (n==r) 
-  {
-    return 1;
-  } 
-  else 
-  {
-    r=(r < n-r) ? n-r : r;
-    return product_Range(r+1, n)/product_Range(1,n-r);
-  }
 }
